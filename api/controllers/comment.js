b825eac147be9705cf214d0c59dd0c968a963306
Mie <dotam1236@gmail.com>
2022-02-14 23:48:43 +0700

    update add main cmt

--- api/controllers/comment.js
+++ api/controllers/comment.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
-const Comment = require('../models/comment');
+const mongoose = require ('mongoose');
+const Comment = require ('../models/comment');
 
 exports.getAllComment = (req, res, next) => {
-  Comment.find()
-    .populate({
+  Comment.find ()
+    .populate ({
       path: 'product',
       select: '_id name',
       populate: {
@@ -11,48 +11,48 @@ exports.getAllComment = (req, res, next) => {
         select: '_id username avatar',
       },
     })
-    .populate({path: 'commentator', select: '_id username avatar'})
-    .populate({
+    .populate ({path: 'commentator', select: '_id username avatar'})
+    .populate ({
       path: 'subComment.sender',
       select: '_id username avatar',
     })
-    .populate({
+    .populate ({
       path: 'subComment.receiver',
       select: '_id username avatar',
     })
-    .select('-__v')
-    .exec()
-    .then((comments) =>
-      res.status(200).json({counter: comments.length, comments})
+    .select ('-__v')
+    .exec ()
+    .then (comments =>
+      res.status (200).json ({counter: comments.length, comments})
     )
-    .catch((error) => console.log(error));
+    .catch (error => console.log (error));
 };
 
-exports.createMainComment = (req, res, next) => {
-  const {product, mainComment} = req.body;
-  const commentator = req.currentUser;
-  let newComment = new Comment({
-    product,
-    mainComment,
-    commentator,
-  });
+exports.addMainCmt = async (req, res) => {
+  try {
+    const {product, mainComment} = req.body;
+    const commentator = req.currentUser;
+    const newComment = new Comment ({
+      product,
+      mainComment,
+      commentator,
+    });
 
-  newComment
-    .save()
-    .then((doc) => {
-      return res.status(200).json({doc, message: 'comment created'});
-    })
-    .catch((error) => res.send(error));
+    const doc = await newComment.save ();
+    return res.status (200).json ({doc, message: 'updated'});
+  } catch (err) {
+    return res.status (500).json ({err});
+  }
 };
 
 exports.getCommentsFromProduct = (req, res, next) => {
   const {productId} = req.params;
-  Comment.find({
+  Comment.find ({
     product: {
       _id: productId,
     },
   })
-    .populate({
+    .populate ({
       path: 'product',
       select: '_id name',
       populate: {
@@ -60,35 +60,35 @@ exports.getCommentsFromProduct = (req, res, next) => {
         select: '_id username avatar',
       },
     })
-    .populate({path: 'commentator', select: '_id username avatar'})
-    .populate({
+    .populate ({path: 'commentator', select: '_id username avatar'})
+    .populate ({
       path: 'subComment.sender',
       select: '_id username avatar',
     })
-    .populate({
+    .populate ({
       path: 'subComment.receiver',
       select: '_id username avatar',
     })
-    .select('-__v')
-    .then((docs) => {
-      return res.status(200).json({docsg});
+    .select ('-__v')
+    .then (docs => {
+      return res.status (200).json ({docsg});
     })
-    .catch((error) => console.log(error));
+    .catch (error => console.log (error));
 };
 
 exports.updateMainComment = async (req, res, next) => {
   const {commentId} = req.params;
   const {mainComment} = req.body;
-  await Comment.findByIdAndUpdate(commentId, {$set: {mainComment}})
-    .exec()
-    .then((doc) => {
-      res.status(200).json({
+  await Comment.findByIdAndUpdate (commentId, {$set: {mainComment}})
+    .exec ()
+    .then (doc => {
+      res.status (200).json ({
         mainComment,
         doc,
       });
     })
-    .catch((error) => {
-      res.status(500).json({
+    .catch (error => {
+      res.status (500).json ({
         error: error,
       });
     });
@@ -96,22 +96,22 @@ exports.updateMainComment = async (req, res, next) => {
 
 exports.getComment = (req, res, next) => {
   const {commentId} = req.params;
-  Comment.findById(commentId)
-    .exec()
-    .then((doc) => {
-      return res.status(200).json({doc});
+  Comment.findById (commentId)
+    .exec ()
+    .then (doc => {
+      return res.status (200).json ({doc});
     })
-    .catch((error) => console.log(error));
+    .catch (error => console.log (error));
 };
 
 exports.deleteMainComment = (req, res, next) => {
   const {commentId} = req.params;
-  Comment.findByIdAndDelete(commentId)
-    .exec()
-    .then((doc) => {
-      return res.status(200).json({doc, message: 'comment was deleted'});
+  Comment.findByIdAndDelete (commentId)
+    .exec ()
+    .then (doc => {
+      return res.status (200).json ({doc, message: 'comment was deleted'});
     })
-    .catch((error) => console.log(error));
+    .catch (error => console.log (error));
 };
 
 exports.createSubComment = (req, res, next) => {
@@ -119,16 +119,16 @@ exports.createSubComment = (req, res, next) => {
   const {content, sender, receiver} = req.body;
   const subContent = {content, sender, receiver};
 
-  Comment.update(
+  Comment.update (
     {_id: commentId},
     {$push: {subComment: subContent}},
     (err, doc) => {
       if (err) {
-        console.log(err);
+        console.log (err);
       }
       return res
-        .status(200)
-        .json({doc, subContent, message: 'comment was updated'});
+        .status (200)
+        .json ({doc, subContent, message: 'comment was updated'});
     }
   );
 };
@@ -137,7 +137,7 @@ exports.updateSubComment = (req, res, next) => {
   const {commentId} = req.params;
   const {newContent} = req.body;
 
-  Comment.update(
+  Comment.update (
     {'subComment._id': commentId},
     {
       $set: {
@@ -145,14 +145,14 @@ exports.updateSubComment = (req, res, next) => {
       },
     }
   )
-    .exec()
-    .then((doc) => {
+    .exec ()
+    .then (doc => {
       return res
-        .status(200)
-        .json({message: 'subComment was updated', newContent, doc});
+        .status (200)
+        .json ({message: 'subComment was updated', newContent, doc});
     })
-    .catch((error) => {
-      return res.status(400).json({error});
+    .catch (error => {
+      return res.status (400).json ({error});
     });
 };
 
@@ -160,17 +160,17 @@ exports.deleteSubComment = (req, res, next) => {
   const {commentId} = req.params;
   const {subCommentId} = req.body;
 
-  Comment.findByIdAndUpdate(
+  Comment.findByIdAndUpdate (
     {_id: commentId},
     {
       $pull: {subComment: {_id: subCommentId}},
     }
   )
-    .exec()
-    .then((doc) => {
-      return res.status(200).json({message: 'subComment was deleted', doc});
+    .exec ()
+    .then (doc => {
+      return res.status (200).json ({message: 'subComment was deleted', doc});
     })
-    .catch((error) => {
-      return res.status(400).json({error});
+    .catch (error => {
+      return res.status (400).json ({error});
     });
 };
