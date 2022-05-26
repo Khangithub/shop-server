c42e10bb2c3094f4135706c52b52a67f023aed9f
Mie <dotam1236@gmail.com>
2022-05-26 19:39:26 +0700

    update chat schema

--- api/models/chat.js
+++ api/models/chat.js
@@ -1,45 +1,38 @@
-const {Schema, model} = require ('mongoose');
+const { Schema, model } = require("mongoose");
 
-const chatSchema = Schema ({
-  from: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  to: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    require: true,
-  },
+const chatSchema = Schema({
+  chatList: [
+    {
+      from: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      content: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: String,
+        required: true,
+      },
+      mediaList: [
+        {
+          filename: { type: String },
+          mimetype: { type: String },
+        },
+      ],
+    },
+  ],
   product: {
     type: Schema.Types.ObjectId,
-    ref: 'Product',
+    ref: "Product",
     require: true,
-  },
-  msg: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    formatedTime: {
-      type: String,
-      require: true,
-    },
-    formatedDate: {
-      type: String,
-      require: true,
-    },
   },
   room: {
     type: String,
     required: true,
   },
-  mediaList: [
-    {
-      filename: {type: String},
-      mimetype: {type: String},
-    },
-  ],
 });
 
-module.exports = model ('Chat', chatSchema);
+module.exports = model("Chat", chatSchema);
