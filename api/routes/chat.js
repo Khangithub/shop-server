618fdd9ad2a393176ec01431ba333df468858dad
Mie <dotam1236@gmail.com>
2022-05-30 23:41:46 +0700

    add auth for getting all chats

--- api/routes/chat.js
+++ api/routes/chat.js
@@ -4,6 +4,6 @@ const { auth, isClient } = require("../middlewares/user");
 const { getMsgs, getChats } = require("../controllers/chat");
 
 router.get("/:roomId", auth, getMsgs);
-router.get("/of/buyer/:userId", getChats);
+router.get("/of/buyer/:userId", auth, isClient, getChats);
 
 module.exports = router;
